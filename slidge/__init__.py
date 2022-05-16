1791ed075e0a8a37e27108d5a1bf7e8d2621adfb
Nicolas Cedilnik <nicolas.cedilnik@inheart.fr>
2022-05-16 07:14:15 +0200

    Monkeypatch XEP-0077 to support data forms

--- slidge/__init__.py
+++ slidge/__init__.py
@@ -1,7 +1,10 @@
+from slixmpp import Iq
+
 from .db import user_store, GatewayUser
-from .gateway import BaseGateway
+from .gateway import BaseGateway, RegistrationField
 from .legacy.base import LegacyContact, BaseLegacyClient
 
+import slixmpp.plugins.xep_0077
 import slixmpp.plugins.xep_0333
 
 
@@ -16,4 +19,100 @@ def send_marker(self, mto, id: str, marker: str, thread=None, *, mfrom=None):
     msg.send()
 
 
+async def _handle_registration(self, iq: Iq):
+    if iq["type"] == "get":
+        await self._send_form(iq)
+    elif iq["type"] == "set":
+        form = iq["register"]["form"]
+        if form:
+            await self.handle_data_form(iq)
+            return
+
+        if iq["register"]["remove"]:
+            try:
+                await self.api["user_remove"](None, None, iq["from"], iq)
+            except KeyError:
+                slixmpp.plugins.xep_0333.register._send_error(
+                    iq,
+                    "404",
+                    "cancel",
+                    "item-not-found",
+                    "User not found",
+                )
+            else:
+                reply = iq.reply()
+                reply.send()
+                self.xmpp.event("user_unregister", iq)
+            return
+
+        for field in self.form_fields:
+            if not iq["register"][field]:
+                # Incomplete Registration
+                slixmpp.plugins.xep_0333.register._send_error(
+                    iq,
+                    "406",
+                    "modify",
+                    "not-acceptable",
+                    "Please fill in all fields.",
+                )
+                return
+
+        try:
+            await self.api["user_validate"](None, None, iq["from"], iq["register"])
+        except ValueError as e:
+            slixmpp.plugins.xep_0333.register._send_error(
+                iq,
+                "406",
+                "modify",
+                "not-acceptable",
+                e.args,
+            )
+        else:
+            reply = iq.reply()
+            reply.send()
+            self.xmpp.event("user_register", iq)
+
+
+async def handle_data_form(self, iq: Iq):
+    form = iq["register"]["form"].get_values()
+    try:
+        remove = form["remove"]
+    except KeyError:
+        pass
+    else:
+        if remove:
+            try:
+                await self.api["user_remove"](None, None, iq["from"], iq)
+            except KeyError:
+                slixmpp.plugins.xep_0333.register._send_error(
+                    iq,
+                    "404",
+                    "cancel",
+                    "item-not-found",
+                    "User not found",
+                )
+            else:
+                reply = iq.reply()
+                reply.send()
+                self.xmpp.event("user_unregister", iq)
+            return
+
+    try:
+        await self.api["user_validate"](None, None, iq["from"], iq)
+    except ValueError as e:
+        slixmpp.plugins.xep_0333.register._send_error(
+            iq,
+            "406",
+            "modify",
+            "not-acceptable",
+            e.args,
+        )
+    else:
+        reply = iq.reply()
+        reply.send()
+        self.xmpp.event("user_register", iq)
+
+
 slixmpp.plugins.xep_0333.XEP_0333.send_marker = send_marker
+slixmpp.plugins.xep_0077.XEP_0077._handle_registration = _handle_registration
+slixmpp.plugins.xep_0077.XEP_0077.handle_data_form = handle_data_form
