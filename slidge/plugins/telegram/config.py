180db7485d93e7abda1aadfc1549a57e9c205a33
nicoco <nicoco@nicoco.fr>
2022-12-20 12:08:43 +0100

    telegram: use new and shiny registration flow
    allow admins to use their api ids and hashes globally

--- slidge/plugins/telegram/config.py
+++ slidge/plugins/telegram/config.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Optional
 
 TDLIB_PATH: Path
 TDLIB_PATH__DOC = "Defaults to ${SLIDGE_HOME_DIR}/tdlib"
@@ -6,3 +7,15 @@ TDLIB_PATH__DYNAMIC_DEFAULT = True
 
 TDLIB_KEY: str = "NOT_SECURE"
 TDLIB_KEY__DOC = "Key used to encrypt tdlib persistent DB"
+
+API_ID: Optional[int] = None
+API_ID__DOC = "Telegram app api_id, obtained at https://my.telegram.org/apps"
+
+API_HASH: Optional[str] = None
+API_HASH__DOC = "Telegram app api_hash, obtained at https://my.telegram.org/apps"
+
+REGISTRATION_AUTH_CODE_TIMEOUT: int = 60
+REGISTRATION_AUTH_CODE_TIMEOUT__DOC = (
+    "On registration, users will be prompted for a 2FA code they receive "
+    "on other telegram clients."
+)
