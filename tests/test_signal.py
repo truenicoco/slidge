01864997d59b6d0199513b16adc491ac4021c135
nicoco <nicoco@nicoco.fr>
2022-12-20 12:13:23 +0100

    signal: use new and shiny registration flow
    Change: registration is now only to create a new account

--- tests/test_signal.py
+++ tests/test_signal.py
@@ -46,103 +46,103 @@ class TestSignalBase(SlidgeTest):
         home_dir = Path(tempfile.mkdtemp())
 
 
-class TestSignalUnregistered(TestSignalBase):
-    def setUp(self):
-        signal = self.signal = slidge.plugins.signal.gateway.signal = MockSignal()
+# class TestSignalUnregistered(TestSignalBase):
+#     def setUp(self):
+#         signal = self.signal = slidge.plugins.signal.gateway.signal = MockSignal()
+#
+#         async def connect_signal_mock(self, socket):
+#             self.signal.set_result(signal)
+#
+#         slidge.plugins.signal.gateway.Gateway.connect_signal = connect_signal_mock
+#
+#         super(TestSignalUnregistered, self).setUp()
+#         self.xmpp["xep_0356"].granted_privileges["test"].message = "outgoing"
+#         self.xmpp["xep_0356"].granted_privileges["test"].roster = "both"
+#
+#     def test_registration_primary_device_missing_name(self):
+#         self.recv(
+#             """
+#             <iq type='set'
+#                 to="signal.test"
+#                 from='romeo@test'>
+#                 <query xmlns='jabber:iq:register'>
+#                     <x xmlns="jabber:x:data" type="form">
+#                         <field var="phone"><value>+123</value></field>
+#                         <field var="device"><value>primary</value></field>
+#                     </x>
+#                 </query>
+#             </iq>
+#             """
+#         )
+#         s = self.next_sent()
+#         assert s["error"]["condition"] == "not-acceptable"
+#
+#     def test_registration_primary_device(self):
+#         self.recv(
+#             """
+#             <iq type='set'
+#                 to="signal.test"
+#                 from='romeo@test'
+#                 id="123">
+#                 <query xmlns='jabber:iq:register'>
+#                     <x xmlns="jabber:x:data" type="form">
+#                         <field var="phone"><value>+123</value></field>
+#                         <field var="device"><value>primary</value></field>
+#                         <field var="name"><value>Romeo</value></field>
+#                     </x>
+#                 </query>
+#             </iq>
+#             """
+#         )
+#         self.send(
+#             """
+#             <iq type="result" to="romeo@test" from="signal.test" id="123"/>
+#             """
+#         )
+#         # self.send(
+#         #     """
+#         #     <presence to="romeo@signal.test" type="subscribe" from="signal.test" />
+#         #     """
+#         # )
+#         assert (
+#             user_store.get(None, None, JID("romeo@test"), None).registration_form[
+#                 "name"
+#             ]
+#             == "Romeo"
+#         )
+#         user_store.remove(None, None, JID("romeo@test"), None)
 
-        async def connect_signal_mock(self, socket):
-            self.signal.set_result(signal)
 
-        slidge.plugins.signal.gateway.Gateway.connect_signal = connect_signal_mock
-
-        super(TestSignalUnregistered, self).setUp()
-        self.xmpp["xep_0356"].granted_privileges["test"].message = "outgoing"
-        self.xmpp["xep_0356"].granted_privileges["test"].roster = "both"
-
-    def test_registration_primary_device_missing_name(self):
-        self.recv(
-            """
-            <iq type='set'
-                to="signal.test"
-                from='romeo@test'>
-                <query xmlns='jabber:iq:register'>
-                    <x xmlns="jabber:x:data" type="form">
-                        <field var="phone"><value>+123</value></field>
-                        <field var="device"><value>primary</value></field>
-                    </x>
-                </query>
-            </iq>
-            """
-        )
-        s = self.next_sent()
-        assert s["error"]["condition"] == "not-acceptable"
-
-    def test_registration_primary_device(self):
-        self.recv(
-            """
-            <iq type='set'
-                to="signal.test"
-                from='romeo@test'
-                id="123">
-                <query xmlns='jabber:iq:register'>
-                    <x xmlns="jabber:x:data" type="form">
-                        <field var="phone"><value>+123</value></field>
-                        <field var="device"><value>primary</value></field>
-                        <field var="name"><value>Romeo</value></field>
-                    </x>
-                </query>
-            </iq>
-            """
-        )
-        self.send(
-            """
-            <iq type="result" to="romeo@test" from="signal.test" id="123"/>
-            """
-        )
-        # self.send(
-        #     """
-        #     <presence to="romeo@signal.test" type="subscribe" from="signal.test" />
-        #     """
-        # )
-        assert (
-            user_store.get(None, None, JID("romeo@test"), None).registration_form[
-                "name"
-            ]
-            == "Romeo"
-        )
-        user_store.remove(None, None, JID("romeo@test"), None)
-
-
-class TestSignalFinalizePrimaryDeviceRegistration(TestSignalBase):
-    def setUp(self):
-
-        super().setUp()
-        jid = JID("romeo@signal.test")
-        user_store.add(jid, {"phone": "+123", "device": "primary", "name": "Romeo"})
-        self.romeo = user_store.get_by_jid(jid)
-        self.signal = slidge.plugins.signal.gateway.signal = MockSignal()
-
-    def tearDown(self):
-        user_store.remove(None, None, self.romeo.jid, None)
-        slidge.plugins.signal.gateway.signal = None
-        super().tearDown()
-
-    def test_register_primary_device(self):
-        subscribe_calls = []
-        register_calls = []
-
-        async def subscribe(account):
-            subscribe_calls.append(account)
-            if len(subscribe_calls) == 1:
-                raise exc.NoSuchAccountError(payload={})
-
-        async def register(account, captcha=None):
-            register_calls.append([account, captcha])
-            if captcha is None:
-                raise exc.CaptchaRequiredError(payload={})
-
-        self.signal.subscribe = subscribe
-        self.signal.register = register
+# class TestSignalFinalizePrimaryDeviceRegistration(TestSignalBase):
+#     def setUp(self):
+#
+#         super().setUp()
+#         jid = JID("romeo@signal.test")
+#         user_store.add(jid, {"phone": "+123", "device": "primary", "name": "Romeo"})
+#         self.romeo = user_store.get_by_jid(jid)
+#         self.signal = slidge.plugins.signal.gateway.signal = MockSignal()
+#
+#     def tearDown(self):
+#         user_store.remove(None, None, self.romeo.jid, None)
+#         slidge.plugins.signal.gateway.signal = None
+#         super().tearDown()
+#
+#     def test_register_primary_device(self):
+#         subscribe_calls = []
+#         register_calls = []
+#
+#         async def subscribe(account):
+#             subscribe_calls.append(account)
+#             if len(subscribe_calls) == 1:
+#                 raise exc.NoSuchAccountError(payload={})
+#
+#         async def register(account, captcha=None):
+#             register_calls.append([account, captcha])
+#             if captcha is None:
+#                 raise exc.CaptchaRequiredError(payload={})
+#
+#         self.signal.subscribe = subscribe
+#         self.signal.register = register
         # login is not triggered by presence available anymore
         # self.recv(
         #     f"""
