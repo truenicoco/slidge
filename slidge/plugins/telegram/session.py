dbc16bd6be1e1554388c49553ecf81535f9cb741
nicoco <nicoco@nicoco.fr>
2023-01-19 20:22:51 +0100

    telegram: remove unused import

--- slidge/plugins/telegram/session.py
+++ slidge/plugins/telegram/session.py
@@ -14,7 +14,6 @@ from slixmpp.exceptions import XMPPError
 from slidge import *
 
 from ...util.types import Chat
-from . import config
 from .client import TelegramClient
 from .contact import Contact, Roster
 from .gateway import Gateway
