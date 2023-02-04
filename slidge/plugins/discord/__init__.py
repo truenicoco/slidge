cf01b0f876746a075286264b72bd258a92aea7bc
nicoco <nicoco@nicoco.fr>
2023-02-04 17:08:56 +0100

    discord: refactor: move contact stuff to dedicated module

--- slidge/plugins/discord/__init__.py
+++ slidge/plugins/discord/__init__.py
@@ -7,6 +7,7 @@ from slixmpp import JID
 from slidge import *
 
 from ... import FormField
+from .contact import Contact
 from .session import Session
 
 
@@ -46,76 +47,6 @@ class Gateway(BaseGateway[Session]):
             await di.Client().login(token)
         except di.LoginFailure as e:
             raise ValueError(str(e))
-
-
-class Contact(LegacyContact[Session, int]):
-    MARKS = False
-
-    @property
-    def discord_user(self) -> di.User:
-        logging.debug("Searching for user: %s", self.legacy_id)
-        if (u := self.session.discord.get_user(self.legacy_id)) is None:
-            raise XMPPError(
-                "item-not-found", text=f"Cannot find the discord user {self.legacy_id}"
-            )
-        return u
-
-    @property
-    def direct_channel_id(self):
-        assert self.discord_user.dm_channel is not None
-        return self.discord_user.dm_channel.id
-
-    async def update_reactions(self, m: di.Message):
-        legacy_reactions = []
-        user = self.discord_user
-        for r in m.reactions:
-            if r.is_custom_emoji():
-                continue
-            assert isinstance(r.emoji, str)
-            async for u in r.users():
-                if u == user:
-                    legacy_reactions.append(r.emoji)
-        self.react(m.id, legacy_reactions)
-
-    async def update_info(self):
-        u = self.discord_user
-        self.name = name = u.display_name
-        if u.avatar:
-            self.avatar = str(u.avatar)
-
-        try:
-            profile = await u.profile()
-        except di.Forbidden:
-            log.debug("Forbidden to fetch the profile of %s", u)
-        except di.HTTPException as e:
-            log.debug("HTTP exception %s when fetch the profile of %s", e, u)
-        else:
-            self.set_vcard(full_name=name, note=profile.bio)
-
-        # TODO: use the relationship here
-        # relationship = u.relationship
-
-    async def send_message(self, message: di.Message):
-        reply_to = message.reference.message_id if message.reference else None
-
-        text = message.content
-        attachments = message.attachments
-        msg_id = message.id
-
-        if not attachments:
-            return self.send_text(text, legacy_msg_id=msg_id, reply_to_msg_id=reply_to)
-
-        last_attachment_i = len(attachments := message.attachments) - 1
-        for i, attachment in enumerate(attachments):
-            last = i == last_attachment_i
-            await self.send_file(
-                file_url=attachment.url,
-                file_name=attachment.filename,
-                content_type=attachment.content_type,
-                reply_to_msg_id=reply_to if last else None,
-                legacy_msg_id=msg_id if last else None,
-                caption=text if last else None,
-            )
 
 
 class Roster(LegacyRoster["Session", Contact, int]):
