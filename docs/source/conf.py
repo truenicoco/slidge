635c23f8290f1ef76a07840c94909c1f74f06eaa
nicoco <nicoco@nicoco.fr>
2023-08-22 23:32:26 +0200

    chore: bump poetry version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "furo"
 html_static_path = ["_static"]
 
 intersphinx_mapping = {
