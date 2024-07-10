bea951c00327bd2b26fa54f1d3b9e3f6d83e932d
nicoco <nicoco@nicoco.fr>
2024-07-10 19:51:15 +0200

    fix: iq privileges for all namespaces
    Fixes: https://todo.sr.ht/~nicoco/slidge/202
    References: https://github.com/processone/ejabberd/issues/3889
    References: https://codeberg.org/poezio/slixmpp/pulls/3541

--- slidge/slixfix/__init__.py
+++ slidge/slixfix/__init__.py
@@ -1,11 +1,12 @@
 # This module contains patches for slixmpp; some have pending requests upstream
 # and should be removed on the next slixmpp release.
-
-# ruff: noqa: F401
+import logging
+from collections import defaultdict
 
 import slixmpp.plugins
 from slixmpp import Message
 from slixmpp.plugins.xep_0050 import XEP_0050, Command
+from slixmpp.plugins.xep_0356.privilege import _VALID_ACCESSES, XEP_0356
 from slixmpp.xmlstream import StanzaBase
 
 from . import (  # xep_0356,
@@ -21,6 +22,37 @@ from . import (  # xep_0356,
     xep_0424,
     xep_0490,
 )
+
+# ruff: noqa: F401
+
+
+# TODO: Remove me once https://codeberg.org/poezio/slixmpp/pulls/3541 makes it
+#       to a slixmpp release
+def _handle_privilege(self, msg: StanzaBase):
+    """
+    Called when the XMPP server advertise the component's privileges.
+
+    Stores the privileges in this instance's granted_privileges attribute (a dict)
+    and raises the privileges_advertised event
+    """
+    permissions = self.granted_privileges[msg.get_from()]
+    for perm in msg["privilege"]["perms"]:
+        access = perm["access"]
+        if access == "iq":
+            if not perm.get_plugin("namespace", check=True):
+                permissions.iq = defaultdict(lambda: perm["type"])
+            else:
+                for ns in perm["namespaces"]:
+                    permissions.iq[ns["ns"]] = ns["type"]
+        elif access in _VALID_ACCESSES:
+            setattr(permissions, access, perm["type"])
+        else:
+            log.warning("Received an invalid privileged access: %s", access)
+    log.debug("Privileges: %s", self.granted_privileges)
+    self.xmpp.event("privileges_advertised")
+
+
+XEP_0356._handle_privilege = _handle_privilege
 
 
 def session_bind(self, jid):
@@ -66,3 +98,4 @@ slixmpp.plugins.PLUGINS.extend(
 
 
 Message.reply = reply  # type: ignore
+log = logging.getLogger(__name__)
