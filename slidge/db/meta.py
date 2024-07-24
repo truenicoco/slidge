a6171a6249504331ec490bf628b3e82d15fbf299
nicoco <nicoco@nicoco.fr>
2024-07-24 06:03:17 +0200

    fix: add naming conventions for SQL constraints
    This should help migration auto-generation…

--- slidge/db/meta.py
+++ slidge/db/meta.py
@@ -58,6 +58,13 @@ JSONSerializable = dict[str, JSONSerializableTypes]
 
 class Base(sa.orm.DeclarativeBase):
     type_annotation_map = {JSONSerializable: JSONEncodedDict, JID: JIDType}
+    naming_convention = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_`%(constraint_name)s`",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
 
 
 def get_engine(path: str) -> sa.Engine:
