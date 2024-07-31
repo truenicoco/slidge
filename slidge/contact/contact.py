truenicoco/slidge

commit 1065d32dc38e1793a429477b45e384f46f3123c5

perf!: write contact data to DB only once, after update_info()
Breaking change: LegacyRoster.fill() must now be an async
iterator yielding contacts that need to be added to the
roster.
During LegacyRoster.fill(), calling LegacyContact.add_to_roster()
is now a no-op, since slidge core will take care of adding
all contacts yielded by LegacyRoster.fill() on startup.

This also allows not doing useless "roster pushes" (via
XEP-0356) for roster items that are not modified.

slidge/contact/contact.py
@@ -11,6 +11,7 @@ from slixmpp.types import MessageTypes
 
 from ..core import config
 from ..core.mixins import AvatarMixin, FullCarbonMixin, StoredAttributeMixin
+from ..core.mixins.db import UpdateInfoMixin
 from ..core.mixins.disco import ContactAccountDiscoMixin
 from ..core.mixins.recipient import ReactionRecipientMixin, ThreadRecipientMixin
 from ..db.models import Contact
@@ -30,6 +31,7 @@ class LegacyContact(
     FullCarbonMixin,
     ReactionRecipientMixin,
     ThreadRecipientMixin,
+    UpdateInfoMixin,
     metaclass=SubclassableOnce,
 ):
     """
@@ -132,8 +134,26 @@ class LegacyContact(
         if value == self._is_friend:
             return
         self._is_friend = value
+        if self._updating_info:
+            return
         assert self.contact_pk is not None
         self.xmpp.store.contacts.set_friend(self.contact_pk, value)
+
+    @property
+    def added_to_roster(self):
+        return self._added_to_roster
+
+    @added_to_roster.setter
+    def added_to_roster(self, value: bool):
+        if value == self._added_to_roster:
+            return
+        self._added_to_roster = value
+        if self._updating_info:
+            return
+        if self.contact_pk is None:
+            # during LegacyRoster.fill()
+            return
+        self.xmpp.store.contacts.set_added_to_roster(self.contact_pk, value)
 
     @property
     def participants(self) -> list["LegacyParticipant"]:
@@ -152,7 +172,7 @@ class LegacyContact(
         return self.session.user_jid
 
     def __repr__(self):
-        return f"<Contact '{self.legacy_id}'/'{self.jid.bare}'>"
+        return f"<Contact {self.jid.bare} - {self.name or self.legacy_id}'>"
 
     def __get_subscription_string(self):
         if self.is_friend:
@@ -198,7 +218,7 @@ class LegacyContact(
             self._privileged_send(stanza)
             return stanza  # type:ignore
 
-        if isinstance(stanza, Presence):
+        if not self._updating_info and isinstance(stanza, Presence):
             self.__propagate_to_participants(stanza)
             if (
                 not self.is_friend
@@ -209,7 +229,11 @@ class LegacyContact(
             n = self.xmpp.plugin["xep_0172"].stanza.UserNick()
             n["nick"] = self.name
             stanza.append(n)
-        if self.xmpp.MARK_ALL_MESSAGES and is_markable(stanza):
+        if (
+            not self._updating_info
+            and self.xmpp.MARK_ALL_MESSAGES
+            and is_markable(stanza)
+        ):
             assert self.contact_pk is not None
             self.xmpp.store.contacts.add_to_sent(self.contact_pk, stanza["id"])
         stanza["to"] = self.user_jid
@@ -244,21 +268,31 @@ class LegacyContact(
     def name(self, n: Optional[str]):
         if self._name == n:
             return
-        for p in self.participants:
-            p.nickname = n
         self._name = n
-        assert self.contact_pk is not None
-        self.xmpp.store.contacts.update_nick(self.contact_pk, n)
         self.xmpp.pubsub.broadcast_nick(
             user_jid=self.user_jid, jid=self.jid.bare, nick=n
         )
-
-    def _get_cached_avatar_id(self):
+        if self._updating_info:
+            # means we're in update_info(), so no participants, and no need
+            # to write to DB now, it will be called in Roster.__finish_init_contact
+            return
+        for p in self.participants:
+            p.nickname = n
         assert self.contact_pk is not None
+        self.xmpp.store.contacts.update_nick(self.contact_pk, n)
+
+    def _get_cached_avatar_id(self) -> Optional[str]:
+        if self.contact_pk is None:
+            return None
         return self.xmpp.store.contacts.get_avatar_legacy_id(self.contact_pk)
 
     def _post_avatar_update(self):
-        assert self.contact_pk is not None
+        if self._updating_info:
+            return
+        if self.contact_pk is None:
+            # happens in LegacyRoster.fill(), the contact primary key is not
+            # set yet, but this will eventually be called in LegacyRoster.__finish_init_contact
+            return
         self.xmpp.store.contacts.set_avatar(self.contact_pk, self._avatar_pk)
         for p in self.participants:
             self.log.debug("Propagating new avatar to %s", p.muc)
@@ -313,6 +347,15 @@ class LegacyContact(
 
         self.xmpp.vcard.set_vcard(self.jid.bare, vcard, {self.user_jid.bare})
 
+    def get_roster_item(self):
+        item = {
+            "subscription": self.__get_subscription_string(),
+            "groups": [self.xmpp.ROSTER_GROUP],
+        }
+        if (n := self.name) is not None:
+            item["name"] = n
+        return {self.jid.bare: item}
+
     async def add_to_roster(self, force=False):
         """
         Add this contact to the user roster using :xep:`0356`
@@ -324,18 +367,10 @@ class LegacyContact(
         if config.NO_ROSTER_PUSH:
             log.debug("Roster push request by plugin ignored (--no-roster-push)")
             return
-        item = {
-            "subscription": self.__get_subscription_string(),
-            "groups": [self.xmpp.ROSTER_GROUP],
-        }
-        if (n := self.name) is not None:
-            item["name"] = n
-        kw = dict(
-            jid=self.user_jid,
-            roster_items={self.jid.bare: item},
-        )
         try:
-            await self._set_roster(**kw)
+            await self._set_roster(
+                jid=self.user_jid, roster_items=self.get_roster_item()
+            )
         except PermissionError:
             warnings.warn(
                 "Slidge does not have privileges to add contacts to the roster. Refer"
@@ -376,9 +411,9 @@ class LegacyContact(
 
     async def _set_roster(self, **kw):
         try:
-            return await self.xmpp["xep_0356"].set_roster(**kw)
+            await self.xmpp["xep_0356"].set_roster(**kw)
         except PermissionError:
-            return await self.xmpp["xep_0356_old"].set_roster(**kw)
+            await self.xmpp["xep_0356_old"].set_roster(**kw)
 
     def send_friend_request(self, text: Optional[str] = None):
         presence = self._make_presence(ptype="subscribe", pstatus=text, bare=True)
@@ -393,7 +428,6 @@ class LegacyContact(
         """
         self.is_friend = True
         assert self.contact_pk is not None
-        self.xmpp.store.contacts.set_friend(self.contact_pk, True)
         self.log.debug("Accepting friend request")
         presence = self._make_presence(ptype="subscribed", pstatus=text, bare=True)
         self._send(presence, nick=True)

slidge/command/user.py
@@ -131,7 +131,6 @@ class ListContacts(Command):
         self, session: Optional[AnyBaseSession], _ifrom: JID, *_
     ) -> TableResult:
         assert session is not None
-        await session.contacts.fill()
         contacts = sorted(
             session.contacts, key=lambda c: c.name.casefold() if c.name else ""
         )

slidge/contact/roster.py
@@ -1,9 +1,10 @@
 import asyncio
 import logging
-from typing import TYPE_CHECKING, Generic, Iterator, Optional, Type
+import warnings
+from typing import TYPE_CHECKING, AsyncIterator, Generic, Iterator, Optional, Type
 
 from slixmpp import JID
-from slixmpp.exceptions import XMPPError
+from slixmpp.exceptions import IqError, XMPPError
 from slixmpp.jid import JID_UNESCAPE_TRANSFORMATIONS, _unescape_node
 
 from ..core.mixins.lock import NamedLockMixin
@@ -64,25 +65,32 @@ class LegacyRoster(
     async def __finish_init_contact(
         self, legacy_id: LegacyUserIdType, jid_username: str, *args, **kwargs
     ):
-        c = self._contact_cls(self.session, legacy_id, jid_username, *args, **kwargs)
-        async with self.lock(("finish", c.legacy_id)):
+        async with self.lock(("finish", legacy_id)):
             with self.__store.session():
                 stored = self.__store.get_by_legacy_id(
                     self.session.user_pk, str(legacy_id)
                 )
-                if stored is not None and stored.updated:
-                    self.log.debug("Already updated %s", c)
-                    return self._contact_cls.from_store(self.session, stored)
-                c.contact_pk = self.__store.add(
-                    self.session.user_pk, c.legacy_id, c.jid
-                )
+                if stored is not None:
+                    if stored.updated:
+                        return self._contact_cls.from_store(self.session, stored)
+                    c: LegacyContact = self._contact_cls(
+                        self.session, legacy_id, jid_username, *args, **kwargs
+                    )
+                    c.contact_pk = stored.id
+                else:
+                    c = self._contact_cls(
+                        self.session, legacy_id, jid_username, *args, **kwargs
+                    )
                 try:
-                    await c.avatar_wrap_update_info()
+                    with c.updating_info():
+                        await c.avatar_wrap_update_info()
                 except Exception as e:
-                    self.log.debug("Deleting %s because of %r", legacy_id, e)
-                    self.__store.delete(c.contact_pk)
                     raise XMPPError("internal-server-error", str(e))
-                self.__store.update(c)
+                c._caps_ver = await c.get_caps_ver(c.jid)
+                need_avatar = c.contact_pk is None
+                c.contact_pk = self.__store.update(c, commit=not self.__filling)
+                if need_avatar:
+                    c._post_avatar_update()
         return c
 
     def known_contacts(self, only_friends=True) -> dict[str, LegacyContactType]:
@@ -195,18 +203,63 @@ class LegacyRoster(
         """
         return _unescape_node(jid_username)
 
-    async def fill(self):
+    async def _fill(self):
+        try:
+            if hasattr(self.session.xmpp, "TEST_MODE"):
+                # dirty hack to avoid mocking xmpp server replies to this
+                # during tests
+                raise PermissionError
+            iq = await self.session.xmpp["xep_0356"].get_roster(
+                self.session.user_jid.bare
+            )
+            user_roster = iq["roster"]["items"]
+        except (PermissionError, IqError):
+            user_roster = None
+
+        with self.__store.session() as orm:
+            self.__filling = True
+            async for contact in self.fill():
+                if user_roster is None:
+                    continue
+                item = contact.get_roster_item()
+                old = user_roster.get(contact.jid.bare)
+                if old is not None and all(
+                    old[k] == item[contact.jid.bare][k]
+                    for k in ("subscription", "groups", "name")
+                ):
+                    self.log.debug("No need to update roster")
+                    continue
+                self.log.debug("Updating roster")
+                try:
+                    await self.session.xmpp["xep_0356"].set_roster(
+                        self.session.user_jid.bare,
+                        item,
+                    )
+                except (PermissionError, IqError) as e:
+                    warnings.warn(f"Could not add to roster: {e}")
+                else:
+                    contact._added_to_roster = True
+            orm.commit()
+        self.__filling = False
+
+    async def fill(self) -> AsyncIterator[LegacyContact]:
         """
         Populate slidge's "virtual roster".
 
-        Override this and in it, ``await self.by_legacy_id(contact_id)``
-        for the every legacy contacts of the user for which you'd like to
-        set an avatar, nickname, vcard…
+        This should yield contacts that are meant to be added to the user's
+        roster, typically by using ``await self.by_legacy_id(contact_id)``.
+        Setting the contact nicknames, avatar, etc. should be in
+        :meth:`LegacyContact.update_info()`
 
-        Await ``Contact.add_to_roster()`` in here to add the contact to the
-        user's XMPP roster.
+        It's not mandatory to override this method, but it is recommended way
+        to populate "friends" of the user. Calling
+        ``await (await self.by_legacy_id(contact_id)).add_to_roster()``
+        accomplishes the same thing, but doing it in here allows to batch
+        DB queries and is better performance-wise.
+
         """
-        pass
+        return
+        yield
 
 
 ESCAPE_TABLE = "".maketrans({v: k for k, v in JID_UNESCAPE_TRANSFORMATIONS.items()})

slidge/core/gateway/base.py
@@ -401,7 +401,7 @@ class BaseGateway(
         log.debug("Context in the exception handler: %s", context)
         exc = context.get("exception")
         if exc is None:
-            log.warning("No exception in this context: %s", context)
+            log.debug("No exception in this context: %s", context)
         elif isinstance(exc, SystemExit):
             log.debug("SystemExit called in an asyncio task")
         else:

slidge/core/mixins/avatar.py
@@ -133,10 +133,11 @@ class AvatarMixin:
         """
         Set an avatar for this entity
 
-        :param a:
-        :param avatar_unique_id:
-        :param blocking:
-        :param cancel:
+        :param a: The avatar, in one of the types slidge supports
+        :param avatar_unique_id: A globally unique ID for the avatar on the
+            legacy network
+        :param blocking: Internal use by slidge for tests, do not use!
+        :param cancel: Internal use by slidge, do not use!
         """
         if avatar_unique_id is None and a is not None:
             avatar_unique_id = self.__get_uid(a)

slidge/core/mixins/db.py
@@ -1,0 +1,18 @@
+from contextlib import contextmanager
+
+
+class UpdateInfoMixin:
+    """
+    This mixin just adds a context manager that prevents commiting to the DB
+    on every attribute change.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._updating_info = False
+
+    @contextmanager
+    def updating_info(self):
+        self._updating_info = True
+        yield
+        self._updating_info = False

slidge/core/mixins/presence.py
@@ -19,27 +19,30 @@ _FRIEND_REQUEST_PRESENCES = {"subscribe", "unsubscribe", "subscribed", "unsubscr
 
 class PresenceMixin(BaseSender):
     _ONLY_SEND_PRESENCE_CHANGES = False
-    contact_pk: Optional[int]
+    contact_pk: Optional[int] = None
 
     def __init__(self, *a, **k):
         super().__init__(*a, **k)
         # FIXME: this should not be an attribute of this mixin to allow garbage
         #        collection of instances
         self.__update_last_seen_fallback_task: Optional[Task] = None
+        # this is only used when a presence is set during Contact.update_info(),
+        # when the contact does not have a DB primary key yet, and is written
+        # to DB at the end of update_info()
+        self.cached_presence: Optional[CachedPresence] = None
 
     async def __update_last_seen_fallback(self):
         await sleep(3600 * 7)
         self.send_last_presence(force=True, no_cache_online=False)
 
     def _get_last_presence(self) -> Optional[CachedPresence]:
-        # TODO: use contact PK instead of JID
         if self.contact_pk is None:
             return None
         return self.xmpp.store.contacts.get_presence(self.contact_pk)
 
     def _store_last_presence(self, new: CachedPresence):
-        # TODO: use contact PK instead of JID
         if self.contact_pk is None:
+            self.cached_presence = new
             return
         self.xmpp.store.contacts.set_presence(self.contact_pk, new)
 

slidge/db/store.py
@@ -292,22 +292,6 @@ class ContactStore(UpdatedMixin):
             session.execute(update(Contact).values(cached_presence=False))
             session.commit()
 
-    def add(self, user_pk: int, legacy_id: str, contact_jid: JID) -> int:
-        with self.session() as session:
-            existing = session.execute(
-                select(Contact)
-                .where(Contact.legacy_id == legacy_id)
-                .where(Contact.jid == contact_jid.bare)
-            ).scalar()
-            if existing is not None:
-                return existing.id
-            contact = Contact(
-                jid=contact_jid.bare, legacy_id=legacy_id, user_account_id=user_pk
-            )
-            session.add(contact)
-            session.commit()
-            return contact.id
-
     def get_all(self, user_pk: int) -> Iterator[Contact]:
         with self.session() as session:
             yield from session.execute(
@@ -394,20 +378,34 @@ class ContactStore(UpdatedMixin):
                 return None
             return contact.avatar.legacy_id
 
-    def update(self, contact: "LegacyContact"):
+    def update(self, contact: "LegacyContact") -> int:
         with self.session() as session:
-            session.execute(
-                update(Contact)
-                .where(Contact.id == contact.contact_pk)
-                .values(
-                    nick=contact.name,
-                    is_friend=contact.is_friend,
-                    added_to_roster=contact.added_to_roster,
-                    updated=True,
-                    extra_attributes=contact.serialize_extra_attributes(),
+            if contact.contact_pk is None:
+                if contact.cached_presence is not None:
+                    presence_kwargs = contact.cached_presence._asdict()
+                    presence_kwargs["cached_presence"] = True
+                else:
+                    presence_kwargs = {}
+                row = Contact(
+                    jid=contact.jid.bare,
+                    legacy_id=str(contact.legacy_id),
+                    user_account_id=contact.user_pk,
+                    **presence_kwargs,
                 )
-            )
+            else:
+                row = (
+                    session.query(Contact)
+                    .filter(Contact.id == contact.contact_pk)
+                    .one()
+                )
+            row.nick = contact.name
+            row.is_friend = contact.is_friend
+            row.added_to_roster = contact.added_to_roster
+            row.updated = True
+            row.extra_attributes = contact.serialize_extra_attributes()
+            session.add(row)
             session.commit()
+            return row.id
 
     def add_to_sent(self, contact_pk: int, msg_id: str) -> None:
         with self.session() as session:

commit d2ff515b34c11facc98d1818fad73ab053509ea7

fix: do not broadcast presences of non-friends
…except friend-request-related ones.

Regression introduced by: 1065d32dc38e1793a429477b45e384f46f3123c5

slidge/contact/contact.py
@@ -282,8 +282,9 @@ class LegacyContact(
             self._privileged_send(stanza)
             return stanza  # type:ignore
 
-        if not self._updating_info and isinstance(stanza, Presence):
-            self.__propagate_to_participants(stanza)
+        if isinstance(stanza, Presence):
+            if not self._updating_info:
+                self.__propagate_to_participants(stanza)
             if (
                 not self.is_friend
                 and stanza["type"] not in self._NON_FRIEND_PRESENCES_FILTER